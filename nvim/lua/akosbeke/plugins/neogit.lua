285c839cd0fbab9614c2c9c17a34848b35029084
Ákos Beke <akosbeke@gmail.com>
2024-08-25 14:11:40 +0200

    Added neogit plugin

---
+++ nvim/lua/akosbeke/plugins/neogit.lua
@@ -0,0 +1,14 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  config = function()
+    local neogit = require("neogit")
+    neogit.setup({})
+
+    vim.keymap.set("n", "<leader>gs", "<cmd>Neogit<cr>", { noremap = true, silent = true })
+  end,
+}
